bfff7274e5e815e4857e61eaded319ba67183e51
izeye <izeye@naver.com>
2014-07-24 20:57:35 +0900

    Change login page.

--- organizeme/src/main/java/com/ctb/organizeme/web/controller/HomeController.java
+++ organizeme/src/main/java/com/ctb/organizeme/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.ctb.organizeme.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,17 @@ public class HomeController {
 	@RequestMapping("/errors/404.html")
 	public String handle404() {
 		return "redirect:/";
+	}
+
+	@RequestMapping("/login.html")
+	public String login() {
+		return "login/login.html";
+	}
+	
+	@RequestMapping("/login-error.html")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login/login.html";
 	}
 
 }
