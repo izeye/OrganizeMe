863d706c8ed0fab877223d7b83dc2f1cb2a85c57
izeye <izeye@naver.com>
2014-08-01 23:53:27 +0900

    Add slide content type.

--- organizeme/src/main/java/com/ctb/organizeme/domain/ContentType.java
+++ organizeme/src/main/java/com/ctb/organizeme/domain/ContentType.java
@@ -2,8 +2,8 @@ package com.ctb.organizeme.domain;
 
 public enum ContentType {
 
-	TEXT, IMAGE, VIDEO;
+	TEXT, SLIDE, IMAGE, VIDEO;
 
-	public static final ContentType[] ALL = { TEXT, IMAGE, VIDEO };
+	public static final ContentType[] ALL = { TEXT, SLIDE, IMAGE, VIDEO };
 
 }
